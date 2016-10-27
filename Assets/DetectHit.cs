f9be60b15183d7c0a7f9b04ffdcd37bd0c7eb169
hundredvisionsguy <winikkc@hsd.k12.or.us>
2016-10-27 16:48:42 -0700

    adding health bar tied to skeleton's health

--- Assets/DetectHit.cs
+++ Assets/DetectHit.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DetectHit : MonoBehaviour {
-
+	public Slider healthbar;
 	//  	
 	void OnTriggerEnter(Collider other) {
-		Debug.Log ("Hit");
+		healthbar.value -= 20;
+		//Debug.Log ("Hit");
 	}
 
 	// Use this for initialization
