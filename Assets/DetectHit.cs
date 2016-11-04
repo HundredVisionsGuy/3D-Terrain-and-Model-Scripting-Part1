99b3971e57c41c6a8d6a3109b01f07b3a33234a2
hundredvisionsguy <winikkc@hsd.k12.or.us>
2016-11-03 17:30:37 -0700

    fixing files from new gitignore and removing .pdb files

---
+++ Assets/DetectHit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DetectHit : MonoBehaviour {
+	public Slider healthbar;
+	Animator anim;
+	public string opponent;
+	//  	
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag != opponent)
+			return;
+		healthbar.value -= 30;
+		if (healthbar.value <= 0)
+			anim.SetBool ("isDead", true);
+		//Debug.Log ("Hit");
+	}
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
